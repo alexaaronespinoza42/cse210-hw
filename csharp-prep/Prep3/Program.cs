f168edcdbb1cba3401ea57e45211d963f446cbb8
alexaaronespinoza42 <131840728+alexaaronespinoza42@users.noreply.github.com>
2023-11-04 09:18:40 -0500

    random number generator

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rndmGen = new Random();
+        int magicNmbr = rndmGen.Next(1,101);
+
+        int guess = -1;
+
+        while (guess != magicNmbr)
+        {
+            Console.WriteLine("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (magicNmbr < guess)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (magicNmbr > guess)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it");
+            }
+        }
+
     }
-}
+}   
