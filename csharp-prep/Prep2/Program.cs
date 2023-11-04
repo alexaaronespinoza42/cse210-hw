bb8bc11cbc29b1df5d49342a5a5d5afb665d37de
alexaaronespinoza42 <131840728+alexaaronespinoza42@users.noreply.github.com>
2023-11-04 08:31:28 -0500

    New percentgrade code

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,6 +5,42 @@ class Program
     static void Main(string[] args)
     {
         //This is prep 2
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("What is your grade percentage? ");
+        string gradePercentage = Console.ReadLine();
+        int percent = int.Parse(gradePercentage);
+
+        string letterGrade = "";
+
+        if (percent >= 90)
+        {
+            letterGrade = "A";
+        }
+        else if (percent >= 80)
+        {
+            letterGrade = "B";
+        }
+        else if (percent >= 70)
+        {
+            letterGrade = "C";
+        }
+        else if (percent >= 60)
+        {
+            letterGrade = "D";
+        }
+        else if (percent < 60)
+        {
+            letterGrade = "F";
+        }
+
+        Console.WriteLine($"Your grade is: {letterGrade}");
+
+        if (percent >= 70){
+            Console.WriteLine("You passed!");
+        }
+        else
+        {
+            Console.WriteLine("You failed!");
+        }
+
     }
 }
