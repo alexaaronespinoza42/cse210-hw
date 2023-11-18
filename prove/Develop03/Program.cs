a2e5ef33e85a8efbad002fed4e2b5bbd2f6eb8c5
alexaaronespinoza42 <131840728+alexaaronespinoza42@users.noreply.github.com>
2023-11-18 16:44:05 -0500

    NEW CODING DEVELOP 03

--- prove/Develop03/Program.cs
+++ prove/Develop03/Program.cs
@@ -1,9 +1,28 @@
 using System;
 
+
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Develop03 World!");
+        Reference reference = new Reference("John", 3, 16);
+        Scripture scripture = new Scripture(reference , "God so loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+            
+        while (!scripture.IsCompletelyHidden())
+        {
+            Console.Clear();
+            Console.WriteLine(scripture.GetDisplayText());
+            Console.WriteLine("\nPress enter to reveal more words of the scripture or type 'quit' to exit the program.");
+                
+            string input = Console.ReadLine();
+
+            if (input.ToLower() == "quit")
+            {
+                break;
+            }
+            scripture.HideRandomWords(3);
+        }
+
+        Console.WriteLine("Take care!");
     }
 }
---
+++ prove/Develop03/Scripture.cs
@@ -0,0 +1,65 @@
+using System;
+using System.Collections.Generic;
+
+class Scripture
+{
+    private Reference _reference;
+    private List<Word> _words;
+
+    public Scripture(Reference reference, string text)
+    {
+        _reference = reference;
+        _words = CreateWordList(text);
+    }
+
+    private List<Word> CreateWordList(string text)
+    {
+        // Split the text into words
+        string[] wordArray = text.Split(' ');
+
+        List<Word> words = new List<Word>();
+        foreach (string wordText in wordArray)
+        {
+            Word word = new Word(wordText);
+            words.Add(word);
+        }
+
+        return words;
+    }
+
+    public void HideRandomWords(int numberToHide)
+    {
+        Random random = new Random();
+
+        for (int i = 0; i < numberToHide; i++)
+        {
+            int index = random.Next(_words.Count);
+            _words[index].Hide();
+        }
+    }
+
+    public string GetDisplayText()
+    {
+        List<string> displayWords = new List<string>();
+
+        foreach (Word word in _words)
+        {
+            displayWords.Add(word.GetDisplayText());
+        }
+
+        return string.Join(" ", displayWords);
+    }
+
+    public bool IsCompletelyHidden()
+    {
+        foreach (Word word in _words)
+        {
+            if (!word.IsHidden())
+            {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
